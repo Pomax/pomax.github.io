ee208f09e7abab71bdb9a75c385b57fd2180a094
Pomax <pomax@nihongoresources.com>
2015-12-06 12:58:47 -0800

    .

--- gh-weblog-2/enriched.js
+++ gh-weblog-2/enriched.js
@@ -3095,27 +3095,17 @@ var React = (window.React);
 var marked = require("../bower_components/marked/lib/marked");
 
 module.exports = React.createClass({displayName: "exports",
-  getInitialState: function() {
-    return {
-      html: {__html: marked(this.props.text)}
-    }
-  },
   componentDidMount: function(prevProps, prevState) {
-    // FIXME: this should be a controlled setting
     var nd = this.refs.post.getDOMNode();
-    console.log(nd);
-    var ccount = nd.children.length;
-    if (ccount < 8) {
-      console.log("entry only has " + ccount + " children, unfolding");
-      this.props.unfold();
-    }
+    console.log(nd.children.length + " children");
   },
   render: function() {
+    var html = {__html: marked(this.props.text)};
     return React.createElement("div", {ref: "post", 
                 className: "post", 
                 hidden: this.props.hidden, 
                 onClick: this.props.onClick, 
-                dangerouslySetInnerHTML: this.state.html})
+                dangerouslySetInnerHTML: html})
   },
   getHTML: function() {
     return this.refs.post.getDOMNode().innerHTML
