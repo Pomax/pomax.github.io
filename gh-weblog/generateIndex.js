5c45aee4cbf5d039122984477ee7e1626e1f3f72
Pomax <pomax@nihongoresources.com>
2015-12-05 18:00:10 -0800

    .

---
+++ gh-weblog/generateIndex.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var path = require('path');
+var dir = process.cwd();
+if (!dir.match('gh-weblog.gh-weblog')) {
+  dir = dir + "/gh-weblog";
+}
+dir += "/content/posts";
+var md = dir + "/markdown";
+var meta = dir + "/metadata";
+
+var index = require(dir + "/index.json");
+if (index.index) {
+  var ids = index.index;
+  delete index.index;
+  ids.forEach(function(id) {
+    var metadata = require(meta + '/' + id + '.json');
+    index[id] = metadata;
+  });
+}
+
+fs.writeFileSync(dir + "/toc.json", JSON.stringify(index,false,2));
